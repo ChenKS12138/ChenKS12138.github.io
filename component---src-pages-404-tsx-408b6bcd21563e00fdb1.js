9dd90762a4ff30aac09e0a61d93e59ef8dd65fcc
ChenKS12138 <42082890+ChenKS12138@users.noreply.github.com>
2020-06-09 04:35:24 +0000

    Deploying to master from  @ 6c00025f6256e351aff0e7cd687bf7baf757730b 🚀

---
+++ component---src-pages-404-tsx-408b6bcd21563e00fdb1.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{"i6+/":function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),l=n.n(a),u=n("Z+rF"),i=n("PzNL");t.default=function(){return l.a.createElement(u.a,null,l.a.createElement(i.a,{title:"404: Not found"}),l.a.createElement("div",{style:{width:"960px",maxWidth:"80vw"}},l.a.createElement("h1",null,"NOT FOUND"),l.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness.")))}}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-408b6bcd21563e00fdb1.js.map
