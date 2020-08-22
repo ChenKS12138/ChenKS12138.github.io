00529ef15e6b3f3a01cf980eaec83093a83f1a2a
ChenKS12138 <749923710@qq.com>
2020-08-22 22:40:02 +0800

    fix: canvas image cross origin

--- src/utils/customHooks.ts
+++ src/utils/customHooks.ts
@@ -1,15 +1,17 @@
 import React, { useState, useEffect } from "react";
 
 /**
- *
+ * 获取图片的平均颜色
  * @param src {string}
  */
 export function useImageColor(src: string): string {
   const [color, setColor] = useState("");
+  const [url] = useNetworkImageData(src);
   useEffect(() => {
+    if (!url) return;
     const img = new Image();
-    img.src = src;
-    img.setAttribute("crossOrigin", "Anonymous");
+    img.src = url;
+    img.setAttribute("crossOrigin", "anonymous");
     const canvas = document.createElement("canvas");
     const ctx = canvas.getContext("2d");
     img.onload = function () {
@@ -26,6 +28,24 @@ export function useImageColor(src: string): string {
       const b = rgba[2] / (img.width * img.height);
       setColor(`rgb(${r},${g},${b})`);
     };
-  }, [src]);
+  }, [url]);
   return color;
+}
+
+/**
+ * @param src {string}
+ */
+export function useNetworkImageData(src: string) {
+  const [blob, setBlob] = useState(null);
+  useEffect(() => {
+    const xhr = new XMLHttpRequest();
+    xhr.onload = function () {
+      const url = URL.createObjectURL(this.response);
+      setBlob(url);
+    };
+    xhr.open("GET", src, true);
+    xhr.responseType = "blob";
+    xhr.send();
+  }, [src]);
+  return [blob];
 }
