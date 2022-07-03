71a39a4bf6d143dc2acf04b547eb4d07bd89f8b7
ChenKS12138 <chenks12138@gmail.com>
2022-07-03 21:21:29 +0800

    feat: font

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,6 +3,7 @@
  *
  * See: https://www.gatsbyjs.org/docs/browser-apis/
  */
+import "@fontsource/open-sans";
 
 require("prism-themes/themes/prism-coldark-cold.css");
 require("github-markdown-css/github-markdown.css");
--- src/components/GlobalStyle.tsx
+++ src/components/GlobalStyle.tsx
@@ -3,5 +3,6 @@ import styled, { createGlobalStyle } from "styled-components";
 export default createGlobalStyle`
   body {
     background-color: rgb(235, 235, 235);
+    font-family: "Open Sans";
   }
 `;
