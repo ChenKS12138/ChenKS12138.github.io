ca024e9b889a605981ac9875424f440b1300de78
ChenKS12138 <749923710@qq.com>
2024-03-10 06:39:29 +0000

    Deploying to master from  @ e25dc15074f4558d4f449383356f82c332c4e72b 🚀

---
+++ assets/404.8364e0a6.js
@@ -0,0 +1,1 @@
+import{d as e,m as t,f as _,o,i as s}from"./app.606baedb.js";const a={class:"hint color-text-comm dark:text-white"},c=e({__name:"404",setup(n){return t({title:"404"}),(d,r)=>(o(),_("div",a,"404. Not Found"))}});const i=s(c,[["__scopeId","data-v-6471a801"]]);export{i as default};
